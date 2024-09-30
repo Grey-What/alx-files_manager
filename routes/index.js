15f3a87ba24c7d62122f457604ad9935c66b3780
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-30 04:24:02 -0700

    eslint fixed

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-import AppController from "../controllers/AppController";
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const addRoutes = (api) => {
-  api.get("/status", AppController.getStatus);
-  api.get("/stats", AppController.getStats);
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
+
+  api.post('/users', UsersController.postNew);
 };
 
 export default addRoutes;
