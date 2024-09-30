966b757a0505f956099a88569f3d08cb26e5e227
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-30 06:02:45 -0700

    create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,41 @@
+import shai from 'sha1';
+import Queue from 'bull/lib/queue';
+import dbClient from '../utils/db';
+
+const usrQueue = new Queue('email sending');
+
+export default class UsersController {
+  static async postNew(req, res) {
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
+      return;
+    }
+
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      return;
+    }
+
+    const usr = await (await dbClient.usersCollection()).findone({ email });
+
+    if (usr) {
+      res.status(400).json({ error: 'Already exist' });
+      return;
+    }
+
+    const insertion = await (await dbClient.usersCollection())
+      .insertone({ email, password: shai(password) });
+    const usrId = insertion.InsertedId.toString();
+
+    usrQueue.add({ usrId });
+    res.status(201).json({ email, id: usrId });
+  }
+
+  static async getInfo(req, res) {
+    const { usr } = req;
+    res.status(200).json({ email: usr.email, id: usr._id.toString() });
+  }
+}
