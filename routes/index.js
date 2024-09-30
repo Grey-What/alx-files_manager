d94cce6fcac1ae085698d11da07345f0ffcb1c09
Vishaylinp <vishaylinparshotam@gmail.com>
2024-09-30 06:45:14 -0700

    Revert "eslint fixed"
    This reverts commit 15f3a87ba24c7d62122f457604ad9935c66b3780.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
-import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
+import AppController from "../controllers/AppController";
 
 const addRoutes = (api) => {
-  api.get('/status', AppController.getStatus);
-  api.get('/stats', AppController.getStats);
-
-  api.post('/users', UsersController.postNew);
+  api.get("/status", AppController.getStatus);
+  api.get("/stats", AppController.getStats);
 };
 
 export default addRoutes;
